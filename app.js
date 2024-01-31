02a1c4d4d974a292408e9c120f7107241f29dfdd
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-01-31 08:57:18 -0300

    commit1

--- app.js
+++ app.js
@@ -38,6 +38,88 @@ db.connect((err) => {
     res.render('index');
   });
 
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
+
+  app.get('/cadastro', (req, res) => {
+    res.render('cadastro');
+  });
+
+  app.get('/posts', (req, res) => {
+    res.render('posts');
+  });
+
+
+  //*
+
+  app.post('/login', (req, res) => {
+    const { name, password } = req.body;
+    const query = 'SELECT * FROM user WHERE name = ? AND password = ?';
+  
+    db.query(query, [name, password], (err, results) => {
+      if (err) {
+        console.error('Erro na consulta SQL:', err);
+        return res.status(500).send('Erro interno. <a href="/login">Tente novamente</a>');
+      }
+  
+      if (results.length > 0) {
+        // Usuário autenticado com sucesso
+        req.session.loggedin = true;
+        req.session.username = name;
+        res.redirect('/posts'); // Redireciona para a página de dashboard ou outra página desejada
+      } else {
+        return res.status(400).send('Credenciais incorretas. <a href="/login">Tente novamente</a>');
+      }
+    });
+  });
+  
+  app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  });
+
+  app.post('/cadastro', (req, res) => {
+  const { name, password } = req.body;
+  const query = 'INSERT INTO user (name, password) VALUES (?, ? )';
+
+  db.query(query, [name, password], (err, result) => {
+    if (err) {
+      console.error('Erro ao cadastrar o usuário:', err);
+      res.status(500).send('Erro ao cadastrar o usuário. <a href="/cadastro">Tente novamente</a>');
+    } else {
+      console.log('Usuário cadastrado com sucesso!');
+      res.status(500).send('Usuário cadastrado com sucesso! <a href="/">voltar</a>');
+    }
+  });
+});
+  
+// Adicione essas linhas ao seu arquivo app.js
+
+// ...
+
+app.get('/posts', (req, res) => {
+
+  const posts = [
+    { title: 'Post 1', content: 'Conteúdo do Post 1' },
+    { title: 'Post 2', content: 'Conteúdo do Post 2' },
+
+  ];
+
+  res.render('posts', { posts });
+});
+
+app.get('/formulario-publicacao', (req, res) => {
+  res.render('form-publicacao');
+});
+
+app.post('/criar-post', (req, res) => {
+
+  res.redirect('/posts');
+});
+
+// ...
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
